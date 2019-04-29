983a34f55c7f2f034459961bac02638e289bef93
nro2 <nro2@pdx.edu>
2019-04-28 19:30:50 -0700

    Finish exercise 1.

--- node/1-lorem.js
+++ node/1-lorem.js
@@ -2,4 +2,16 @@
 
 var http = require('http'); // do not change this line
 
+var server = http.createServer(function(req, res) {
+    res.writeHead(200, {
+        'Content-Type':'text/html'
+    });
+    res.write('lorem ipsum');
+    res.end();
+  });
+ 
+  server.listen(process.env.PORT || 8080);
+ 
+
+  
 // any request should return '<!DOCTYPE html><html><body>lorem ipsum</body></html>' as html
