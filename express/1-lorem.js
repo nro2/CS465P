e8acc254ec9ae08c4d69a9ed18132d50bfce60f9
nro2 <nro2@pdx.edu>
2019-05-09 19:15:10 -0700

    Work on express exercise 1.

--- express/1-lorem.js
+++ express/1-lorem.js
@@ -2,4 +2,13 @@
 
 var express = require('express'); // do not change this line
 
+var server = express();
+
+server.get('lorem', function(req, res) {
+    res.status(200);
+    res.write('<!DOCTYPE html><html><body>lorem ipsum</body></html>')
+    res.end();
+})
+
+server.listen(8080);
 // http://localhost:8080/lorem should return '<!DOCTYPE html><html><body>lorem ipsum</body></html>' as html
